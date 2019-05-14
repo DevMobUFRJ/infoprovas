fd17c5e1f3a06e9e6bae9c00d984766d85a607aa
pksasso <patricksassorj@gmail.com>
2019-05-14 14:27:15 -0300

    * Icone do GitHub na tela Sobre

--- lib/styles/github_icon.dart
+++ lib/styles/github_icon.dart
@@ -2,5 +2,5 @@ import 'package:flutter/widgets.dart';
 
 class Git{
   Git._();
-  static const IconData github = IconData(0xf113, fontFamily: 'github');
+  static const IconData github = IconData(0xf09b, fontFamily: 'github');
 }
