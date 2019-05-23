31fa036a3de5de4db1b0cd3a74dcb919fecbda57
pksasso <patricksassorj@gmail.com>
2019-05-23 11:01:29 -0300

    * Layout provas salvas

--- lib/widgets/exam_tile.dart
+++ lib/widgets/exam_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:infoprovas/model/exam.dart';
 import 'package:infoprovas/utils/database_helper.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:infoprovas/screens/exam_view.dart';
+import 'package:infoprovas/styles/style.dart';
 
 class ExamTile extends StatefulWidget {
   final Exam _exam;
@@ -14,7 +16,6 @@ class ExamTile extends StatefulWidget {
 }
 
 class _ExamTileState extends State<ExamTile> {
-
   // remove prova do sqflite
   void _deleteExam() async {
     await DatabaseHelper.internal().deleteExam(widget._exam.id);
@@ -28,39 +29,62 @@ class _ExamTileState extends State<ExamTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Slidable(
-        key: Key(widget._exam.id.toString()),
-        direction: Axis.horizontal,
-        dismissal: SlidableDismissal(
-          child: SlidableDrawerDismissal(),
-          onDismissed: (actionType) {
-            // TODO: implementar snackbar por deletar prova (no ListTile tambem)
+    return Slidable(
+      key: Key(widget._exam.id.toString()),
+      direction: Axis.horizontal,
+      dismissal: SlidableDismissal(
+        child: SlidableDrawerDismissal(),
+        onDismissed: (actionType) {
+          // TODO: implementar snackbar por deletar prova (no ListTile tambem)
+          _deleteExam();
+        },
+      ),
+      actionPane: SlidableScrollActionPane(),
+      actionExtentRatio: 0.2,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 0, top: 0),
+          child: ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Style.mainTheme.primaryColor,
+              ),
+              child: Center(
+                child: Text(
+                  dateFormat(
+                      "${widget._exam.year}", "${widget._exam.semester}"),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+              ),
+            ),
+            title: Text(
+              "${widget._exam.subject}",
+            ),
+            subtitle: Text("${widget._exam.professorName}"),
+            onTap: openExamView,
+          ),
+        ),
+      ),
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'Remover',
+          color: Colors.red[400],
+          icon: Icons.delete,
+          onTap: () {
             _deleteExam();
           },
         ),
-        actionPane: SlidableScrollActionPane(),
-        actionExtentRatio: 0.2,
-        child: ListTile(
-          title: Text(
-            "${widget._exam.subject} - ${widget._exam.professorName}",
-          ),
-          subtitle: Text(
-            "${widget._exam.type} - ${widget._exam.year}.${widget._exam.semester}",
-          ),
-          onTap: openExamView,
-        ),
-        secondaryActions: <Widget>[
-          IconSlideAction(
-            caption: 'Remover',
-            color: Colors.red[400],
-            icon: Icons.delete,
-            onTap: () {
-              _deleteExam();
-            },
-          ),
-        ],
-      ),
+      ],
     );
   }
+}
+
+String dateFormat(String year, String semester) {
+  return "${year.substring(2, 4)}.${semester}";
 }
