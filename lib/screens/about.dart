1bd2ace0c29d9875884448b64407dfdff06f34ae
Patrick Sasso <patricksassorj@gmail.com>
2019-08-10 12:04:55 -0300

    * Tela sobre arrumada

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -40,103 +40,101 @@ class About extends StatelessWidget {
         title: Text("Sobre"),
         elevation: 2.0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Expanded(
-              child: ListView(
-                physics: ScrollPhysics(parent: BouncingScrollPhysics()),
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Image.asset(
-                        "assets/devmob-logo.png",
-                        width: 80,
-                      ),
-                    ],
-                  ),
-                  Container(
-                    height: 8.0,
-                  ),
-                  Text(
-                    "App desenvolvido por DevMob UFRJ",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                  Divider(
-                    height: 28,
-                  ),
-                  Text(
-                    "Infoprovas",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Style.mainTheme.primaryColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 6.0),
-                    child: Text(
-                      "O InfoProvas é um projeto do CAInfo, Centro Acadêmico do curso de Ciência da Computação. "
-                      "Anteriormente, consistindo apenas de um site.\nO InfoProvas conta com provas antigas de "
-                      "professores e disciplinas, com o objetivo principal de auxiliar os alunos nos estudos, "
-                      "permitindo uma preparação mais efetiva e objetiva, trazendo resultados melhores pros alunos, "
-                      "pro curso, e pra universidade.",
-                      textAlign: TextAlign.justify,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black45,
-                    height: 28,
-                  ),
-                  Text(
-                    "DevMob",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Style.mainTheme.primaryColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 6.0),
-                    child: Text(
-                      "O aplicativo foi desenvolvido pelo DevMob, Grupo de Desenvolvimento de Aplicativos do "
-                      "Departamento de Ciência da Computação da UFRJ. O app foi desenvolvido em Flutter, e o seu "
-                      "código fonte está aberto, disponível no GitHub do grupo.",
-                      textAlign: TextAlign.justify,
-                    ),
-                  ),
-                  Divider(
-                    color: Colors.black45,
-                    height: 28,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(Facebook.facebook),
-                        iconSize: 40,
-                        onPressed: () => _launchURL(
-                            context, "http://facebook.com/devmobufrj"),
-                      ),
-                      IconButton(
-                        icon: Icon(Git.github),
-                        iconSize: 40,
-                        onPressed: () => _launchURL(
-                            context, "https://github.com/DevMobUFRJ"),
-                      ),
-                    ],
+      body: Container(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(parent: BouncingScrollPhysics()),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(height: 16.0,),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset(
+                    "assets/devmob-logo.png",
+                    width: 80,
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 8.0,
+              ),
+              Text(
+                "App desenvolvido por DevMob UFRJ",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              Divider(
+                color: Colors.black45,
+                height: 28,
+              ),
+              Text(
+                "Infoprovas",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Style.mainTheme.primaryColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 6.0),
+                child: Text(
+                  "O InfoProvas é um projeto do CAInfo, Centro Acadêmico do curso de Ciência da Computação. "
+                  "Anteriormente, consistindo apenas de um site.\nO InfoProvas conta com provas antigas de "
+                  "professores e disciplinas, com o objetivo principal de auxiliar os alunos nos estudos, "
+                  "permitindo uma preparação mais efetiva e objetiva, trazendo resultados melhores pros alunos, "
+                  "pro curso, e pra universidade.",
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              Divider(
+                color: Colors.black45,
+                height: 28,
+              ),
+              Text(
+                "DevMob",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Style.mainTheme.primaryColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 6.0),
+                child: Text(
+                  "O aplicativo foi desenvolvido pelo DevMob, Grupo de Desenvolvimento de Aplicativos do "
+                  "Departamento de Ciência da Computação da UFRJ. O app foi desenvolvido em Flutter, e o seu "
+                  "código fonte está aberto, disponível no GitHub do grupo.",
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              Divider(
+                color: Colors.black45,
+                height: 28,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Facebook.facebook),
+                    iconSize: 40,
+                    onPressed: () => _launchURL(
+                        context, "http://facebook.com/devmobufrj"),
+                  ),
+                  IconButton(
+                    icon: Icon(Git.github),
+                    iconSize: 40,
+                    onPressed: () => _launchURL(
+                        context, "https://github.com/DevMobUFRJ"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
